054cd95ce5a4b16fd16c274d7ea4bcbd1b4cbcab
Mateusz <ryniuss@o2.pl>
2023-01-14 14:05:32 +0100

    added featured

--- src/components/featured/Featured.js
+++ src/components/featured/Featured.js
@@ -5,14 +5,22 @@ import Bed1 from '../../assets/bed1.jpg';
 import Bed2 from '../../assets/bed2.jpg';
 import Kitchen from '../../assets/kitchen.jpg';
 import Bathroom from '../../assets/bath1.jpg';
+import House2 from '../../assets/house2.jpg';
+import Bed3 from '../../assets/bed3.jpg';
+import Bed4 from '../../assets/bed4.jpg';
+import Bathroom2 from '../../assets/bath2.jpg';
+import LivingRoom from '../../assets/living-room.jpg';
+import './Feeatured.css';
 
 const Featured = () => {
 	return (
 		<div className='featured'>
 			<h1 className='featured-text'>Top Featured Listings</h1>
-			<p>Selected listings by City, State, & Zip based on views.</p>
+			<p className='featured-text'>
+				Selecte Listings by City, State, & Zip based on views.
+			</p>
 			<div className='container'>
-				<img src={House1} alt='' />
+				<img className='span-3 image-grid-row-2' src={House1} alt='' />
 				<img src={Bed1} alt='' />
 				<img src={Bed2} alt='' />
 				<img src={Kitchen} alt='' />
@@ -34,8 +42,60 @@ const Featured = () => {
 								<p>7</p>
 							</div>
 						</div>
+						<div>
+							<div className='info'>
+								<p className='bold'>Square Feet:</p>
+								<p>8,138</p>
+							</div>
+							<div className='info'>
+								<p className='bold'>Est Payment:</p>
+								<p>$14,797/mo</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className='span-2 right-img-details'>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
+						omnis unde dolores sed, sapiente, repudiandae excepturi illum
+						laboriosam quaerat voluptatum ipsa quo necessitatibus! Repellendus
+						quae?
+					</p>
+					<button className='btn'>View Listing</button>
+				</div>
+			</div>
+			<div className='container'>
+				<img src={Bed3} alt='' />
+				<img src={Bed4} alt='' />
+				<img className='span-3 image-grid-row-2' src={House2} alt='' />
+				<img src={Bathroom2} alt='' />
+				<img src={LivingRoom} alt='' />
+				<div className='span-2 right-img-details'>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque,
+						omnis unde dolores sed, sapiente, repudiandae excepturi illum
+						laboriosam quaerat voluptatum ipsa quo necessitatibus! Repellendus
+						quae?
+					</p>
+					<button className='btn'>View Listing</button>
+				</div>
+				<div className='span-3 img-details'>
+					<div className='top'>
+						<h2>123 Acme St. Dallas, TX</h2>
+						<p>House for Sale</p>
+						<p className='price'>$2,677,000</p>
 					</div>
 					<div className='info-grid'>
+						<div>
+							<div className='info'>
+								<p className='bold'>Bedrooms:</p>
+								<p>5</p>
+							</div>
+							<div className='info'>
+								<p className='bold'>Bathrooms:</p>
+								<p>7</p>
+							</div>
+						</div>
 						<div>
 							<div className='info'>
 								<p className='bold'>Square Feet:</p>
