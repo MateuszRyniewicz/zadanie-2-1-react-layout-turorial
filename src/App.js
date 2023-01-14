5e922c9f8d325fac986b4891011672df2b1039f8
Mateusz <ryniuss@o2.pl>
2023-01-14 11:03:27 +0100

    added section the  featured

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Best from './components/best/Best';
+import Featured from './components/featured/Featured';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 
@@ -9,8 +10,11 @@ const App = () => {
 			<Navbar />
 			<Hero />
 			<Best />
+			<Featured />
 		</>
 	);
 };
 
 export default App;
+
+
