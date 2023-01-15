fff873fd8e44a877be308c40cdcdb9b55e3901b1
Mateusz <ryniuss@o2.pl>
2023-01-15 08:53:02 +0100

    page is done includes css

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Best from './components/best/Best';
 import Featured from './components/featured/Featured';
+import Footer from './components/footer/Footer';
 import Hero from './components/hero/Hero';
 import Navbar from './components/navbar/Navbar';
 
@@ -11,10 +12,9 @@ const App = () => {
 			<Hero />
 			<Best />
 			<Featured />
+			<Footer />
 		</>
 	);
 };
 
 export default App;
-
-
